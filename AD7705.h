5bc2de5825f02f77210707c625f0dd70659ceeb6
sjzMa <42200676+sjzMa@users.noreply.github.com>
2019-01-29 00:12:23 +0800

    Update AD7705.h

--- AD7705.h
+++ AD7705.h
@@ -17,13 +17,15 @@
 #define CH_1                 0x01 // _ _ _ _ _ _ 0 1
 
 //Clock Regsiter
+//Note : CANNOT MIX
+///-----------Type 1--------------
 #define CLK_4_9152M          0x0C // _ _ _ _ 1 1 _ _
 #define CLK_2_4576M          0x04 // _ _ _ _ 0 1 _ _
 #define FS_50HZ              0x00 // _ _ _ _ _ _ 0 0
 #define FS_60HZ              0x01 // _ _ _ _ _ _ 0 1
 #define FS_250HZ             0x02 // _ _ _ _ _ _ 1 0
 #define FS_500HZ             0x03 // _ _ _ _ _ _ 1 1
-////--------------
+///-----------Type 2--------------
 #define CLK_2_0000M          0x08 // _ _ _ _ 1 0 _ _ 
 #define CLK_1_0000M          0x00 // _ _ _ _ 0 0 _ _
 #define FS_20HZ              0x00 // _ _ _ _ _ _ 0 0
@@ -32,7 +34,7 @@
 #define FS_200HZ             0x03 // _ _ _ _ _ _ 1 1
 
 //SETTING Regsiter
-#define GENER_CALIBRATE      0x00 // 0 0 _ _ _ _ _ _
+#define NO_CALIBRATE         0x00 // 0 0 _ _ _ _ _ _
 #define SELF_CALIBRATE       0x40 // 0 1 _ _ _ _ _ _
 #define ZERO_CALIBRATE       0x80 // 1 0 _ _ _ _ _ _
 #define FULL_CALIBRATE       0xC0 // 1 1 _ _ _ _ _ _
@@ -49,7 +51,7 @@
 #define DOUBLE_POLAR         0x00 // _ _ _ _ _ 0 _ _
 #define SINGAL_POLAR         0x04 // _ _ _ _ _ 1 _ _
 
-#define CLOSE_BUFFER         0x02 // _ _ _ _ _ _ 0 _ 
+#define CLOSE_BUFFER         0x00 // _ _ _ _ _ _ 0 _ 
 #define OPEN_BUFFER          0x02 // _ _ _ _ _ _ 1 _     
 
 #define FSYNC                0x00 // _ _ _ _ _ _ _ 0
